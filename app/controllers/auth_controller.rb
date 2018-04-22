f56ee8438d9d46fd7c57c169ad6c4d3dfd285570
johnvoyage <jmyork762@gmail.com>
2018-04-22 10:47:55 -0400

    sign in re-renders

--- app/controllers/auth_controller.rb
+++ app/controllers/auth_controller.rb
@@ -2,7 +2,7 @@ class AuthController < ApplicationController
 
   def create
     user = User.find_by(email: params[:email])
-    if user.authenticate(params[:password])
+    if user && user.authenticate(params[:password])
       render json: { email: user.email, id: user.id }
     else
       render json: { error: 'User is invalid' }, status: 401
