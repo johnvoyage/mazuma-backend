a6a8daa2385dc79da8cfca8f66fab070104d6c1a
johnvoyage <jmyork762@gmail.com>
2018-04-23 17:07:08 -0400

    sign up half done

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,4 +6,25 @@ class UsersController < ApplicationController
     render json: @users
   end
 
+  def show
+    @user = User.find_by(slug: params[:slug])
+    render json: @user
+  end
+
+  def create
+    byebug
+    # user = User.new(user_params)
+    # if user.save
+    #   render json: user
+    # else
+    #   render json: {error: 'there was an error'}
+    # end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:email, :password)
+  end
+
 end
