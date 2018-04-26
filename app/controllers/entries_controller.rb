195dacbc701dec6288484ee34f166b7ed12bca60
johnvoyage <jmyork762@gmail.com>
2018-04-26 15:59:28 -0400

    starting entry persistence

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -1,2 +1,34 @@
 class EntriesController < ApplicationController
+
+  def index
+    @entries = Entry.all
+    render json: @entries
+  end
+  def show
+    # byebug
+    @entry = Entry.find(params[:id])
+    render json: @entry
+  end
+
+  def create
+    # byebug
+    entry = Entry.new(entry_params)
+    if entry.save
+      render json: entry
+    else
+      render json: {error: 'there was an error'}
+    end
+  end
+
+  private
+
+  def entry_params
+    params.require(:entry).permit(:date, :description, :user_id)
+  end
+
 end
+
+# t.datetime :date
+# t.text :description
+# t.integer :user_id
+# t.timestamps
--- db/migrate/20180421215422_create_transactions.rb
+++ db/migrate/20180421215422_create_transactions.rb
@@ -1,7 +1,6 @@
 class CreateTransactions < ActiveRecord::Migration[5.2]
   def change
     create_table :transactions do |t|
-      t.string :account
       t.decimal :amount, precision: 10, scale: 2
       t.integer :entry_id
       t.integer :account_id
